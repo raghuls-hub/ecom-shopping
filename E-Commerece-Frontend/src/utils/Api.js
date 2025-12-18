38e0927f79e37d9dcefe57a1bcd705207c95c664
raghuls-hub <raghul.m2024aids@sece.ac.in>
2025-12-18 18:32:20 +0530

    Update Api.js

--- E-Commerece-Frontend/src/utils/Api.js
+++ E-Commerece-Frontend/src/utils/Api.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-export const API = 'http://localhost:5000'
-=======
-export const API = 'https://navi-shopping-ruav.onrender.com'
->>>>>>> 9b1bdf7ebc927119df31b748efff4b38a2e6a23d
+export const API = 'http://51.20.81.27:5000'
